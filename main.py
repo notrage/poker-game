9932ff7b3cfa53f8e382ca4e86c97a960fe72d46
NotRage <grousson.dylan@gmail.com>
2023-05-26 18:27:27 +0200

    Added Bet and Player Class

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from utils.card import Card
 from utils.card_pack import CardPack
 from utils.community import Community
 from utils.hand import Hand
+from utils.player import Player
+from utils.bet import Bet
 
 if __name__ == "__main__":
     
@@ -15,12 +17,20 @@ if __name__ == "__main__":
     main3: Hand = Hand(paquet_de_carte.get_and_remove_mulitple_random_cards(2))
     main4: Hand = Hand(paquet_de_carte.get_and_remove_mulitple_random_cards(2))
     
+    joueur1: Player = Player("Maurice", 2000, main1)
+    
+    mise1: Bet = Bet(joueur1, 1000)
+    
     print(f"le paquet de carte contient {len(paquet_de_carte.__card_list__())} cartes")
     print(f"le paquet de carte contient les cartes suivantes:\n{paquet_de_carte.__str__()}\n")
 
-    print(f"Les cartes ouvertes sont: {cartes_communes.__str__()}\n")
+    print(f"Le flop est composé des cartes : {cartes_communes.__str__()}\n")
     
     print(f"La main1 est composé des cartes: {main1.__str__()}")
     print(f"La main2 est composé des cartes: {main2.__str__()}")
     print(f"La main3 est composé des cartes: {main3.__str__()}")
-    print(f"La main4 est composé des cartes: {main4.__str__()}")
+    print(f"La main4 est composé des cartes: {main4.__str__()}\n")
+    
+    print(f"Le joueur1 a les caractéristiques suivantes: {joueur1.__str__()}\n")
+    
+    print(f"La mise1 a les caractéristique suivantes: {mise1.__str__()}\n")
---
+++ utils/bet.py
@@ -0,0 +1,22 @@
+from utils.player import Player
+
+class Bet:
+    
+    def __init__(self, bet_player: Player, bet_value: int) -> None:
+        
+        self.player: Player = bet_player
+        
+        if bet_value > self.player.__money__():
+            print("Error: cannot create a bet with higher amount of money that player's has")
+            exit()
+            
+        self.value: int = bet_value
+    
+    def __player__(self) -> Player:
+        return self.player
+    
+    def __value__(self) -> int:
+        return self.value
+    
+    def __str__(self) -> str:
+        return f"joueur: {self.__player__().__name__()}, mise: {self.__value__()}"
